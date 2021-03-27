5d143f51a2e755a210f1cc4a1c1057a81198150b
chandu1811 <ramachandarg75@gmail.com>
2021-03-27 16:31:35 +0530

    new changes

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     user=models.OneToOneField(User,null=True,blank=True,on_delete=models.CASCADE)
-    user1 = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name=models.CharField(max_length=100,null=True)
     phone=models.CharField(max_length=100,null=True)
     email=models.CharField(max_length=100,null=True)
